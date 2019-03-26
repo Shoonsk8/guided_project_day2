6f30e9c628b0c54433438778ad31f8bbc08bdd19
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-26 11:10:00 -0700

    Update to end of day 2 code

--- app/src/main/java/com/joshuahalvorson/journal/JournalDetails.java
+++ app/src/main/java/com/joshuahalvorson/journal/JournalDetails.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,22 +22,22 @@ public class JournalDetails extends AppCompatActivity {
     private EditText journalEntry;
     private ImageView journalImage;
     private SeekBar dayRating;
-    private Button addImageButton;
+    private Button addImageButton, shareEntryButton;
 
     private JournalEntry entry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(getLocalClassName(), "onCreate");
         setContentView(R.layout.activity_journal_details);
-
-        entry = (JournalEntry) getIntent().getSerializableExtra(JournalEntry.TAG);
 
         dateText = findViewById(R.id.journal_date);
         journalEntry = findViewById(R.id.journal_entry);
         journalImage = findViewById(R.id.journal_image);
         dayRating = findViewById(R.id.journal_day_rating);
         addImageButton = findViewById(R.id.journal_add_image);
+        shareEntryButton = findViewById(R.id.share_entry_button);
 
         if(entry != null){
             dateText.setText(entry.getDate());
@@ -53,22 +54,86 @@ public class JournalDetails extends AppCompatActivity {
                 startActivityForResult(imageIntent, IMAGE_REQUEST_CODE);
             }
         });
+
+        shareEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, journalEntry.getText().toString());
+                startActivity(Intent.createChooser(sharingIntent, "Share Using"));
+            }
+        });
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if(Intent.ACTION_SEND.equals(action) && type != null){
+            if(type.startsWith("image/")){
+                entry = new JournalEntry(ListActivity.nextId++);
+                Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                entry.setImageUrl(uri);
+                journalImage.setVisibility(View.VISIBLE);
+                journalImage.setImageURI(uri);
+            }
+        }else{
+            entry = (JournalEntry) getIntent().getSerializableExtra(JournalEntry.TAG);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(getLocalClassName(), "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(getLocalClassName(), "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(getLocalClassName(), "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(getLocalClassName(), "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(getLocalClassName(), "onDestroy");
     }
 
     @Override
     public void onBackPressed() {
         Intent resultIntent = new Intent();
-        JournalEntry newEntry = new JournalEntry(entry.getDate(), journalEntry.getText().toString(), "", dayRating.getProgress(), entry.getId());
+        JournalEntry newEntry = new JournalEntry(
+                entry.getDate(),
+                journalEntry.getText().toString(),
+                "",
+                dayRating.getProgress(),
+                entry.getId());
         resultIntent.putExtra(JournalEntry.TAG, newEntry);
         setResult(Activity.RESULT_OK, resultIntent);
         finish();
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
             if(data != null){
                 Uri uri = data.getData();
+                entry.setImageUrl(uri);
                 journalImage.setImageURI(uri);
                 journalImage.setVisibility(View.VISIBLE);
             }
--- app/src/main/java/com/joshuahalvorson/journal/ListActivity.java
+++ app/src/main/java/com/joshuahalvorson/journal/ListActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ import java.util.Date;
 
 public class ListActivity extends AppCompatActivity {
     public static final int EDIT_ENTRY_REQUEST_CODE = 2;
+    public static final int NEW_ENTRY_REQUEST_CODE = 1;
 
     private ArrayList<JournalEntry> journalEntries;
 
@@ -27,12 +29,15 @@ public class ListActivity extends AppCompatActivity {
 
     private LinearLayout entryList;
 
+    public static int nextId = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.i(getLocalClassName(), "onCreate");
 
         entryList = findViewById(R.id.entry_list);
 
@@ -45,18 +50,33 @@ public class ListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                entryList.addView(generateTextView(new JournalEntry("03/23/19", "text", "awdaD", 5, 1)));
+                Intent intent = new Intent(context, JournalDetails.class);
+                JournalEntry journalEntry = createJournalEntry();
+                intent.putExtra(JournalEntry.TAG, journalEntry);
+                startActivityForResult(intent, NEW_ENTRY_REQUEST_CODE);
             }
         });
     }
 
+    public JournalEntry createJournalEntry(){
+        JournalEntry journalEntry = new JournalEntry(nextId++);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        journalEntry.setDate(dateFormat.format(date));
+        return journalEntry;
+    }
 
+    public JournalEntry createJournalEntry(String text){
+        JournalEntry journalEntry = createJournalEntry();
+        journalEntry.setEntryText(text);
+        return journalEntry;
+    }
 
     private TextView generateTextView(final JournalEntry entry){
         TextView textView = new TextView(context);
         textView.setText(String.format("Date: %s Rating: %d", entry.getDate(), entry.getDayRating()));
         textView.setTextColor(Color.BLACK);
-        textView.setTextSize(30);
+        //textView.setTextSize(30);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,9 +89,15 @@ public class ListActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(getLocalClassName(), "onStart");
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-
+        Log.i(getLocalClassName(), "onResume");
         entryList.removeAllViews();
         for(JournalEntry entry: journalEntries) {
             entryList.addView(generateTextView(entry));
@@ -79,20 +105,44 @@ public class ListActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(getLocalClassName(), "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(getLocalClassName(), "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(getLocalClassName(), "onDestroy");
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == EDIT_ENTRY_REQUEST_CODE && resultCode == RESULT_OK){
             if(data != null){
                 JournalEntry entry = (JournalEntry) data.getSerializableExtra(JournalEntry.TAG);
-                journalEntries.set(entry.getId() - 1, entry);
+                journalEntries.set(entry.getId(), entry);
+            }
+        }else if(requestCode == NEW_ENTRY_REQUEST_CODE && resultCode == RESULT_OK){
+            if(data != null){
+                JournalEntry entry = (JournalEntry) data.getSerializableExtra(JournalEntry.TAG);
+                journalEntries.add(entry);
             }
         }
     }
 
     private void addTestEntries() {
-        journalEntries.add(new JournalEntry("03/23/19", "entry1", "awdaD", 5, 1));
-        journalEntries.add(new JournalEntry("03/22/19", "entry2", "awdaD", 3, 2));
-        journalEntries.add(new JournalEntry("03/24/19", "entry3", "awdaD", 4, 3));
-        journalEntries.add(new JournalEntry("03/21/19", "entry4", "awdaD", 1, 4));
+        journalEntries.add(createJournalEntry("Gathered by gravity how far away finite but unbounded the only home we've ever known network of wormholes Jean-François Champollion? Tendrils of gossamer clouds Orion's sword extraplanetary invent the universe trillion stirred by starlight. Shores of the cosmic ocean vastness is bearable only through love permanence of the stars astonishment a mote of dust suspended in a sunbeam extraplanetary. Made in the interiors of collapsing stars not a sunrise but a galaxyrise a very small stage in a vast cosmic arena a mote of dust suspended in a sunbeam something incredible is waiting to be known astonishment."));
+        journalEntries.add(createJournalEntry("Vangelis muse about Hypatia explorations hundreds of thousands another world. Shores of the cosmic ocean a mote of dust suspended in a sunbeam colonies Tunguska event finite but unbounded shores of the cosmic ocean? Extraplanetary bits of moving fluff gathered by gravity a still more glorious dawn awaits not a sunrise but a galaxyrise with pretty stories for which there's little good evidence. Take root and flourish courage of our questions vastness is bearable only through love paroxysm of global death invent the universe something incredible is waiting to be known?"));
+        journalEntries.add(createJournalEntry("Preserve and cherish that pale blue dot two ghostly white figures in coveralls and helmets are soflty dancing vastness is bearable only through love Euclid permanence of the stars inconspicuous motes of rock and gas. Dispassionate extraterrestrial observer something incredible is waiting to be known star stuff harvesting star light great turbulent clouds network of wormholes the only home we've ever known. Of brilliant syntheses emerged into consciousness vanquish the impossible vanquish the impossible hundreds of thousands dream of the mind's eye."));
+        journalEntries.add(createJournalEntry("Extraplanetary Euclid Hypatia brain is the seed of intelligence intelligent beings Rig Veda. Vastness is bearable only through love circumnavigated emerged into consciousness white dwarf colonies something incredible is waiting to be known. Two ghostly white figures in coveralls and helmets are soflty dancing star stuff harvesting star light bits of moving fluff invent the universe concept of the number one the ash of stellar alchemy. The only home we've ever known invent the universe rich in heavy atoms concept of the number one muse about something incredible is waiting to be known."));
+        journalEntries.add(createJournalEntry("Science dream of the mind's eye stirred by starlight Jean-François Champollion with pretty stories for which there's little good evidence circumnavigated? Sea of Tranquility extraordinary claims require extraordinary evidence the carbon in our apple pies the ash of stellar alchemy ship of the imagination preserve and cherish that pale blue dot. Sea of Tranquility hundreds of thousands ship of the imagination the sky calls to us invent the universe descended from astronomers and billions upon billions upon billions upon billions upon billions upon billions upon billions."));
     }
 
 }
